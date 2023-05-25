2fbe3625c4a15ba95cb1bd64f26c55273f0d5b49
hembee <hembee999@gmail.com>
2023-05-25 22:03:11 +0100

    mongo atlass

--- app.js
+++ app.js
@@ -3,25 +3,30 @@ const morgan = require("morgan");
 const globalErrorHandler = require("./src/utils/globalErrorHandler");
 const mongoose = require("mongoose");
 const { userRouter } = require("./src/router/user.route");
-// const config = require("./src/config/index");
+const config = require("./src/config/index");
 require("dotenv").config();
 
+
 // Database connection
-const mongoURI = process.env.MONGODB_CONNECTION_URL;
+const mongoURI = config.MONGODB_CONNECTION_URL;
 
 mongoose
   .connect(mongoURI)
   .then(console.log("Database connection is established"))
   .catch((err) => console.log(err.message));
-const port = process.env.PORT;
+const port = config.PORT;
 const app = express();
 
 // Middleware
 app.use(morgan("tiny"));
 app.use(express.json());
 
+
+
 // Routes
 app.use("/api/v1/user", userRouter);
+
+
 
 // error handler
 app.use(globalErrorHandler);
