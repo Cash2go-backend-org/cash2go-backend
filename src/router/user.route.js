d282c3d21aa4b676626e78fc8c0356421090983f
Dark_Tony <Hembee999@gmail.com>
2023-06-01 09:49:51 +0100

    Update user.route.js

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -29,7 +29,7 @@ router.post(
   tryCatchHandler(passwordController.verifyEmailController)
 );
 router.get(
-  "/securityQuestion",
+  "/security-question",
   tryCatchHandler(userController.getSecurityQuestion)
 );
 
