9acaacf8a1bacce5eaf2721e2cfc2a213face024
hembee <hembee999@gmail.com>
2023-06-02 21:38:12 +0100

    applicant

--- src/model/applicant.model.js
+++ src/model/applicant.model.js
@@ -1,29 +1,15 @@
-const { mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 const applicantSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
+  firstName: String,
+  lastName:  String,
   gender: String,
   DOB: String,
   address: String,
   stateOfOrigin: String,
   adressOfEmployer: String,
-  phoneNumber: {
-    type: Number,
-  },
-  nextOfKinPhoneNumber: {
-    type: Number,
-    validators: {
-      match: [
-        /^[+]{1}(?:[0-9\-\(\)\/\.]\s?){6, 15}[0-9]{1}$/,
-        "Please add a valid phone number with a country code to the phone number path.",
-      ],
-    },
-  },
+  phoneNumber:  Number,
+  nextOfKinPhoneNumber: Number,
   others: String,
 });
 
