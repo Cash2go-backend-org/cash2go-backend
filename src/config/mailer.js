a6060de6eb43584aef14e3162a64a111329c0caf
hembee <hembee999@gmail.com>
2023-06-03 20:33:45 +0100

    token

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -5,7 +5,7 @@ const CLIENT_ID =
 const CLIENT_SECRET = "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";
 const REDIRECT_URL = "https://developers.google.com/oauthplayground";
 const REFRESH_TOKEN =
-  "L9Ircld_ZicZaa34qVoJxjQUIG5TLrBlgS5H7KcTD6px336f8_MO54p50CcvAB__aE5avgg";
+  "1//04jqMCSFFRD7jCgYIARAAGAQSNwF-L9Ir5mXpA8tLOOk0jj6KbazRvmYj7rFY8ncbFCjt3edrykuHaecW6rK7zjscMHT2_njSCHk";
 // Set up OAuth2 credentials
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
