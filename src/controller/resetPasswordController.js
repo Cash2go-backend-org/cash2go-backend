4c2681be41434b8623fd4ce8880ab01ffa024d66
hembee <hembee999@gmail.com>
2023-06-03 22:12:13 +0100

    token

--- src/controller/resetPasswordController.js
+++ src/controller/resetPasswordController.js
@@ -82,7 +82,7 @@ const passwordController = {
       // Send the reset link to the user's email
       const transporter = nodemailer.createTransport(mailerConfig);
 
-      const resetLink = `https://sparkling-mousse-072041.netlify.app/new-password/${resetToken}`;
+      const resetLink = `https://sparkling-mousse-072041.netlify.app/update-password/${resetToken}`;
 
       const mailOptions = {
         from: "hembee999@gmail.com",
