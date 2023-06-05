70e86cba5bd6ccfa51c5932383caded9d29ff3af
Dark_Tony <Hembee999@gmail.com>
2023-06-05 10:26:21 +0100

    Update applicant.controller.js

--- src/controller/applicant.controller.js
+++ src/controller/applicant.controller.js
@@ -49,7 +49,7 @@ const applicantController = {
   },
 
   getApprovedApplicants: async (req, res) => {
-    const approvedApplicants = await Applicant.find({
+    const approvedApplicants = await Prediction.find({
       isApproved: true,
     });
 
