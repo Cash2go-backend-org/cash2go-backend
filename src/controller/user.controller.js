1fd1b12760323dbed83fb3a183c8eea7bd67c7ab
Dark_Tony <Hembee999@gmail.com>
2023-06-14 19:00:39 +0100

    Update user.controller.js

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -41,6 +41,7 @@ const userController = {
 
     // Generate OTP
     const otp = Math.floor(Math.random() * 8888 + 1000);
+    
     // Send OTP email
     await transporter.sendMail({
       from: "hembee999@gmail.com",
