4d16340f36926797377d2bc65c9908a42e112134
hembee <hembee999@gmail.com>
2023-06-28 07:42:44 +0100

    added date and id to applicant model

--- src/model/applicant.model.js
+++ src/model/applicant.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const moment = require('moment');
 const applicantContactSchema = require("./applicantContact.model");
 const predictionSchema = require("./prediction.model");
 
