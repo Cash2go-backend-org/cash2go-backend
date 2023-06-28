52718dd781a5de2d1d668811bc4098dda1a9e8db
hembee <hembee999@gmail.com>
2023-06-28 07:18:09 +0100

    added date and id to applicant model

--- src/model/applicant.model.js
+++ src/model/applicant.model.js
@@ -2,10 +2,29 @@ const mongoose = require("mongoose");
 const applicantContactSchema = require("./applicantContact.model");
 const predictionSchema = require("./prediction.model");
 
-
 const ApplicantSchema = new mongoose.Schema({
   contact: applicantContactSchema,
   prediction: predictionSchema,
+  applicationDate: {
+    type: Date,
+    default: Date.now,
+  },
+  applicationID: {
+    type: Number,
+    unique: true,
+    required: true,
+    min: 100000000,
+    max: 999999999,
+  },
+});
+
+ApplicantSchema.pre("save", function (next) {
+  if (!this.applicationID) {
+    this.applicationID = Math.floor(Math.random() * 900000000 + 100000000);
+  }
+  this.applicationDate = moment(this.applicationDate).format("DD/MM/YY");
+
+  next();
 });
 
 const Applicant = mongoose.model("Applicant", ApplicantSchema);
