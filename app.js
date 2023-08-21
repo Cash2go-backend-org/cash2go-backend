aface5134eb802ad45b9761494a5d89fc7b1db93
Dark_Tony <Hembee999@gmail.com>
2023-08-21 15:43:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const config = require("./src/config/index");
 const checkInternetConnection = require("./src/error/checkInternetConnection");
 require("dotenv").config();
 // const useragent = require("express-useragent");
-// Database connectionnn
+// Database connectionnnn
 const mongoURI = config.MONGODB_CONNECTION_URL;
 
 mongoose
